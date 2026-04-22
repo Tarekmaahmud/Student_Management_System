7126259cee7f0a5000ae34fb249bd02783d4175f
Tarek Mahmud <shantopixel7pro@gmail.com>
2026-04-22 18:57:21 +0600

    Add home view

---
+++ views/home.php
@@ -0,0 +1,9 @@
+<div class="hero">
+    <h2>Welcome to Student Management System</h2>
+    <p>Efficiently manage student information digitally with our secure and user-friendly platform.</p>
+    <div style="margin-top: 30px;">
+        <a href="?page=login" class="btn btn-primary">Get Started</a>
+        <a href="?page=register" class="btn btn-success">Create Account</a>
+        <a href="?page=register_student" class="btn btn-warning">Register as Student</a>
+    </div>
+</div>
